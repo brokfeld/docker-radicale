9e139982dfd8b2d5cfaf907a0eb055cacfe503eb
Jens Brokfeld <geofreak.de@gmail.com>
2022-10-26 20:51:10 +0200

    Fallback USER_UID, USER_GID and LICENSE

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,22 @@
 #!/bin/sh
 
-# add user
+# set USER_UID, if not set
+if [[ -z ${USER_UID} ]];
+then
+  USER_UID=2050
+fi
+
+# set USER_GID, if not set
+if [[ -z ${USER_GID} ]];
+then
+  USER_GID=2050
+fi
+
+# add user for exec
 adduser radicale --system --uid $USER_UID
 addgroup radicale --system --gid $USER_GID
 
-# set permissions
+# set dir permissions
 chown -R radicale:radicale /app/data
 chmod 700 -R /app/data
 
